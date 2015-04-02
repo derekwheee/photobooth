9e074ccd16fa6aa52de6da1c19ac81e8ec9714f9
Derek Wheelden <derek.wheelden@gmail.com>
2015-04-02 20:39:29 +0000

    Basic detect/capture set up

---
+++ index.js
@@ -0,0 +1,68 @@
+var raspi = require('raspi-io'),
+    exec = require('child_process').exec,
+    fs = require('fs'),
+    five = require('johnny-five'),
+    board = new five.Board({
+        io: new raspi()
+    });
+
+board.on('ready', function () {
+
+    var release = {
+            focus : new five.Pin('GPIO5'),
+            shutter : new five.Pin('GPIO6')
+        };
+
+    release.shutter.low();
+    release.focus.low();
+
+    function primeCamera (cb) {
+        release.focus.high();
+
+        board.wait(500, function () {
+            release.focus.low();
+        });
+          
+        board.wait(1000, function () {
+            release.focus.high();
+        });
+
+        board.wait(1500, function () {
+            release.focus.low();
+            if (typeof cb === 'function') cb();
+        });
+    }
+
+    function detect () {
+        primeCamera(function () {
+            exec('gphoto2 --auto-detect', function (err, stdout, stderr) {
+                if (err || stderr) {
+                    console.log('An error occurred');
+                    return false;
+                } else {
+                    console.log(stdout);
+                }
+            });
+        });
+    }
+
+    function capture (download) {
+        primeCamera(function () {
+            exec('gphoto2 --capture-image' + (download ? '-and-download' : ''), function (err, stdout, stderr) {
+                if (err || stderr) {
+                    console.log('An error occurred');
+                } else {
+                    console.log(stdout);
+                    fs.rename('capt0000.jpg', (new Date()).getTime() + '.jpg'); 
+                }
+            });
+        });
+    }
+
+    this.repl.inject({
+        release : release,
+        detect : detect,
+        capture : capture
+    });
+
+});
