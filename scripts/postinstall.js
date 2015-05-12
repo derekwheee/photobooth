f49be1e93058706b96087967018ea91fd7e3f466
Derek Wheelden <derek.wheelden@gmail.com>
2015-05-12 14:48:54 -0400

    Switching postinstall to use spawn

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,51 +1,124 @@
-var exec = require('child_process').exec;
-var os = require('os');
+var spawn = require('child_process').spawn;
+var os    = require('os');
 var chalk = require('chalk');
-var isPi = os.platform() === 'linux' && os.arch() === 'arm';
+var isPi  = os.platform() === 'linux' && os.arch() === 'arm';
 var isOsx = os.platform() === 'darwin' && os.arch() === 'x64';
+var rpiSpawn;
+var gphotoSpawn;
+var gmagickSpawn;
 
 if (isPi) {
     console.log(chalk.bgMagenta('Installing raspi-io'));
-    exec('npm install raspi-io', function (err, stdout, stderr) {
-        if (err) {
+    rpiSpawn = spawn('npm', ['install', 'raspi-io']);
+
+    rpiSpawn.stdout.on('data', function (data) {
+        console.log(data);
+    });
+
+    rpiSpawn.stderr.on('data', function (data) {
+        console.log(chalk.bgRed('ERROR'), chalk.red(data));
+    });
+
+    rpiSpawn.on('close', function (code) {
+        if (!code) {
             console.log(chalk.bgRed('ERROR'), chalk.red('raspi-io could not be installed.'));
         } else {
-            console.log(chalk.bgGreen('SUCCESS'), chalk.green('raspi-io was successfully installed.'))
+            console.log(chalk.bgGreen('SUCCESS'), chalk.green('raspi-io was successfully installed.'));
         }
+        installGphoto();
+    });
+
+    function installGphoto () {
         console.log(chalk.bgMagenta('Installing gphoto2'));
-        exec('sudo ./gphoto2-updater.sh', function (err, stdout, stderr) {
-            if (err) {
+
+        gphotoSpawn = spawn('sudo ./gphoto2-updater.sh');
+
+        gphotoSpawn.stdout.on('data', function (data) {
+            console.log(data);
+        });
+
+        gphotoSpawn.stderr.on('data', function (data) {
+            console.log(chalk.bgRed('ERROR'), chalk.red(data));
+        });
+
+        gphotoSpawn.on('close', function (code) {
+            if (!code) {
                 console.log(chalk.bgRed('ERROR'), chalk.red('gphoto2 could not be installed.'));
             } else {
-                console.log(chalk.bgGreen('SUCCESS'), chalk.green('gphoto2 was successfully installed.'))
+                console.log(chalk.bgGreen('SUCCESS'), chalk.green('gphoto2 was successfully installed.'));
             }
-            console.log(chalk.bgMagenta('Installing GraphicsMagick'));
-            exec('sudo apt-get graphicsmagick', function (err, stdout, stderr) {
-                if (err) {
-                    console.log(chalk.bgRed('ERROR'), chalk.red('GraphicsMagick could not be installed.'));
-                } else {
-                    console.log(chalk.bgGreen('SUCCESS'), chalk.green('GraphicsMagick was successfully installed.'))
-                }
-            });
+            installGmagick();
         });
-    });
+    }
+
+    function installGmagick () {
+        console.log(chalk.bgMagenta('Installing graphicsmagick'));
+
+        gmagickSpawn = spawn('sudo apt-get graphicsmagick');
+
+        gmagickSpawn.stdout.on('data', function (data) {
+            console.log(data);
+        });
+
+        gmagickSpawn.stderr.on('data', function (data) {
+            console.log(chalk.bgRed('ERROR'), chalk.red(data));
+        });
+
+        gmagickSpawn.on('close', function (code) {
+            if (!code) {
+                console.log(chalk.bgRed('ERROR'), chalk.red('graphicsmagick could not be installed.'));
+            } else {
+                console.log(chalk.bgGreen('SUCCESS'), chalk.green('graphicsmagick was successfully installed.'));
+                console.log(chalk.bgGreen('DONE'), chalk.green('Dependencies successfully installed.'));
+            }
+        });
+    }
+
+
 }
 
 if (isOsx) {
     console.log(chalk.bgMagenta('Installing gphoto2'));
-    exec('brew install gphoto2', function (err, stdout, stderr) {
-        if (err) {
+
+    gphotoSpawn = spawn('brew', ['install', 'gphoto2']);
+
+    gphotoSpawn.stdout.on('data', function (data) {
+        console.log(data);
+    });
+
+    gphotoSpawn.stderr.on('data', function (data) {
+        console.log(chalk.bgRed('ERROR'), chalk.red(data));
+    });
+
+    gphotoSpawn.on('close', function (code) {
+        if (!code) {
             console.log(chalk.bgRed('ERROR'), chalk.red('gphoto2 could not be installed.'));
         } else {
-            console.log(chalk.bgGreen('SUCCESS'), chalk.green('gphoto2 was successfully installed.'))
+            console.log(chalk.bgGreen('SUCCESS'), chalk.green('gphoto2 was successfully installed.'));
         }
-        console.log(chalk.bgMagenta('Installing GraphicsMagick'));
-        exec('brew install graphicsmagick', function (err, stdout, stderr) {
-            if (err) {
-                console.log(chalk.bgRed('ERROR'), chalk.red('GraphicsMagick could not be installed'));
+        installGmagick();
+    });
+
+    function installGmagick () {
+        console.log(chalk.bgMagenta('Installing graphicsmagick'));
+
+        gmagickSpawn = spawn('brew', ['install', 'graphicsmagick']);
+
+        gmagickSpawn.stdout.on('data', function (data) {
+            console.log(data);
+        });
+
+        gmagickSpawn.stderr.on('data', function (data) {
+            console.log(chalk.bgRed('ERROR'), chalk.red(data));
+        });
+
+        gmagickSpawn.on('close', function (code) {
+            if (!code) {
+                console.log(chalk.bgRed('ERROR'), chalk.red('graphicsmagick could not be installed.'));
             } else {
-                console.log(chalk.bgGreen('SUCCESS'), chalk.green('GraphicsMagick was successfully installed.'))
+                console.log(chalk.bgGreen('SUCCESS'), chalk.green('graphicsmagick was successfully installed.'));
+                console.log(chalk.bgGreen('DONE'), chalk.green('Dependencies successfully installed.'));
             }
         });
-    });
+    }
 }
